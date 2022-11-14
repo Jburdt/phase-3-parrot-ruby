462ee6021bfc9a95c4acc4e7dc00e038eb009bfa
Josh Burd <Jburdt@gmail.com>
2022-11-13 19:58:24 -0500

    commit tree 1632981c30ff4cef7ed6a977211bfd911de9d918

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,19 @@
+require "pry"
+
+# def parrot (word = "Squawk!")
+#     word
+#     puts word
+#     return "Squawk!"
+# end
+# puts parrot("Pretty bird!")
+
+def parrot (word = "Squawk!")
+    # puts "#{word}"
+    # return "#{word}"
+    puts word
+    return word
+end
+
+
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
